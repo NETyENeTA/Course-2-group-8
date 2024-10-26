8941a6de6c92d3ac340072cef7e047b0092781de
NETyENeTA <gigasekond2@gmail.com>
2024-10-26 20:59:54 +0500

    Task(Lesson 8 [A & B]): I have done 2 practice: A and B

--- Course/2nd year/Lesson8/PracticeAB/Program.cs
+++ Course/2nd year/Lesson8/PracticeAB/Program.cs
@@ -1,34 +1,61 @@
-﻿namespace PracticeAB;
+﻿using System;
+using System.Linq;
+
+namespace PracticeAB;
 
 class Program
 {
 
-    
+
     // Творим тут
 
 
     //1
-
+    private static int Add(int a, int b) => a + b;
 
     //2
-
+    private static bool IsEven(int number) => number % 2 == 0;
 
     //3
-
+    private static string ReversString(string s) => new(s.Reverse().ToArray());
 
     //4
+    private static int FindMax(int[] arr) => arr.Max();
 
+    //5
+    private static int factorial(int sallary) => sallary * 12;
 
-    // 5
-
-
-    // 6
-
+    //6
+    private static double CelsiusToFahrenheit(int celsius) => celsius * 1.8 + 32;
 
     //7
-
+    private static int CountVowels(string s, string vowels = "аиоуеэ")
+    {
+        int result = 0;
+        foreach (char word in s.ToLower()) if (vowels.Contains(word)) result++;
+        return result;
+    }
 
     //8
+    private static int? GeneratePassword(string passToHack)
+    {
+        int count = 0;
+        for (int x = 0; x < 10; x++)
+            for (int y = 0; y < 10; y++)
+                for (int z = 0; z < 10; z++)
+                    for (int h = 0; h < 10; h++)
+                    {
+                        count++;
+                        if (x.ToString() + y.ToString() + z.ToString() + h.ToString() == passToHack)
+                        {
+                            Console.WriteLine("Ура! Вы взломали пароль теперь вы хакер");
+                            return count;
+                        }
+
+                    }
+        return null;
+    }
+
 
     static void Main(string[] args)
     {
