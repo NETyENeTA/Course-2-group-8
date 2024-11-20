da379458bd68167eab8349b7657b94949ed49115
NETyENeTA <gigasekond2@gmail.com>
2024-11-20 16:38:08 +0500

    Task(Lesson13): It's Done!

---
+++ Course/2nd year/Lesson13/PracticeAB/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Testy;
+
+public class FileManager
+{
+    private readonly string Path;
+
+    public FileManager(string filePath) => this.Path = filePath;
+
+    public void Write(string text, bool append = false)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(Path, append)) writer.Write(text);
+        }
+        catch (Exception ex) { Console.WriteLine($"Write [Error]! cause:{ex}."); }
+    }
+
+    public string Read()
+    {
+        try
+        {
+            if (File.Exists(Path)) using (StreamReader reader = new StreamReader(Path)) return reader.ReadToEnd();
+        }
+        catch (Exception ex) { Console.WriteLine($"Read [Error]! cause:{ex}."); }
+        Console.WriteLine("File hasn't found!");
+        return string.Empty;
+    }
+
+    public void RemoveFile() => File.Delete(Path);
+
+
+    public static void Write(string path, string text, bool append = false)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(path, append)) writer.Write(text);
+        }
+        catch (Exception ex) { Console.WriteLine($"Write [Error]! cause:{ex}."); }
+    }
+
+    public static string Read(string path)
+    {
+        try
+        {
+            if (File.Exists(path)) using (StreamReader reader = new StreamReader(path)) return reader.ReadToEnd();
+        }
+        catch (Exception ex) { Console.WriteLine($"Read [Error]! cause:{ex}."); }
+        Console.WriteLine("File hasn't found!");
+        return string.Empty;
+    }
+
+    public static void RemoveFile(string path) => File.Delete(path);
+
+
+}
+
+public class MyJSON <T>
+{
+    private FileManager fileManager { get; set; }
+
+    private static JsonSerializerOptions options = new()
+    {
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+        WriteIndented = true
+    };
+
+    public MyJSON(string path)
+    {
+        fileManager = new FileManager(path);
+    }
+
+    public T? Read() => JsonSerializer.Deserialize<T>(fileManager.Read());
+
+    public static T? Read(string path) => JsonSerializer.Deserialize<T>(FileManager.Read(path));
+
+    public void Write(T obj) => fileManager.Write(JsonSerializer.Serialize(obj, options));
+
+    public static void Write(T obj, string path) => FileManager.Write(path, JsonSerializer.Serialize(obj, options));
+
+}
+
+
+class Person
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public string? email { get; set; }
+    public bool isVeridied { get; set; }
+
+    public string ToString(string type) => type.Replace("%0%", id.ToString()).Replace("%1%", name).Replace("%2%", email).Replace("3", isVeridied.ToString());
+
+}
+
+class PersonAdditional
+{
+    public string? orderId { get; set; }
+    public string? customerName { get; set; }
+    public double totalPrice { get; set; }
+    public List<string>? items { get; set; }
+    public string ToString(string type) => type.Replace("%0%", orderId).Replace("%1%", customerName).Replace("%2%", totalPrice.ToString()).Replace("%3%", GetItems());
+
+    private string GetItems()
+    {
+        string total = "\n";
+        foreach (string item in items!) total += item + "\n";
+        return total;
+    }
+}
+
+
+class Library
+{
+    public string? libraryName { get; set; }
+    public List<Book>? books { get; set; }
+    
+}
+
+class Book
+{
+    public string? title { get; set; }
+    public string? author { get; set; }
+    public int year { get; set; }
+}
+
+
+public class Program
+{
+    public static void PracticeAB()
+    {
+        // PracticeA
+        MyJSON<PersonAdditional> AnnaJson = new("2.json");
+
+        Person Ivan = MyJSON<Person>.Read("1.json")!;
+        PersonAdditional Anna = AnnaJson.Read()!;
+
+        Console.WriteLine(Ivan.ToString("Email:%2%, ID:%0%"));
+        Console.WriteLine(Anna.ToString("items:%3%"));
+
+        //PracticeB
+        Anna.items!.Add("Салфетки для мониторв");
+        Anna.totalPrice += 1555;
+        Anna.totalPrice -= Anna.totalPrice * 0.02d;
+
+        AnnaJson.Write(Anna);
+
+        MyJSON<Library> LibraryJson = new("3.json");
+
+        Library library = LibraryJson.Read()!;
+
+        library.books!.Add(new()
+        {
+            title = "Капитанская дочка",
+            author = "Александр Сергеевич Пушкин",
+            year = 1836
+        });
+
+        LibraryJson.Write(library);
+
+    }
+
+    public static void Main()
+    {
+        PracticeAB();
+    }
+
+}
---
+++ Course/2nd year/Lesson13/PracticeC/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Testy;
+
+public class FileManager
+{
+    private readonly string Path;
+
+    public FileManager(string filePath) => this.Path = filePath;
+
+    public void Write(string text, bool append = false)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(Path, append)) writer.Write(text);
+        }
+        catch (Exception ex) { Console.WriteLine($"Write [Error]! cause:{ex}."); }
+    }
+
+    public string Read()
+    {
+        try
+        {
+            if (File.Exists(Path)) using (StreamReader reader = new StreamReader(Path)) return reader.ReadToEnd();
+        }
+        catch (Exception ex) { Console.WriteLine($"Read [Error]! cause:{ex}."); }
+        Console.WriteLine("File hasn't found!");
+        return string.Empty;
+    }
+
+    public void RemoveFile() => File.Delete(Path);
+
+
+    public static void Write(string path, string text, bool append = false)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(path, append)) writer.Write(text);
+        }
+        catch (Exception ex) { Console.WriteLine($"Write [Error]! cause:{ex}."); }
+    }
+
+    public static string Read(string path)
+    {
+        try
+        {
+            if (File.Exists(path)) using (StreamReader reader = new StreamReader(path)) return reader.ReadToEnd();
+        }
+        catch (Exception ex) { Console.WriteLine($"Read [Error]! cause:{ex}."); }
+        Console.WriteLine("File hasn't found!");
+        return string.Empty;
+    }
+
+    public static void RemoveFile(string path) => File.Delete(path);
+
+
+}
+
+public class MyJSON <T>
+{
+    private FileManager fileManager { get; set; }
+
+    private static JsonSerializerOptions options = new()
+    {
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+        WriteIndented = true
+    };
+
+    public MyJSON(string path)
+    {
+        fileManager = new FileManager(path);
+    }
+
+    public T? Read() => JsonSerializer.Deserialize<T>(fileManager.Read());
+
+    public static T? Read(string path) => JsonSerializer.Deserialize<T>(FileManager.Read(path));
+
+    public void Write(T obj) => fileManager.Write(JsonSerializer.Serialize(obj, options));
+
+    public static void Write(T obj, string path) => FileManager.Write(path, JsonSerializer.Serialize(obj, options));
+
+}
+
+
+class Company
+{
+    public string? companyName { get; set; }
+    public List<Employee>? employees { get; set; }
+    public Dictionary<string, string>? location { get; set; }
+
+    public void AddEmployee(Employee employee) => employees?.Add(employee);
+
+    public Employee? GetEmployee(int id)
+    {
+        foreach (Employee employee in employees!) if (employee.id == id) return (Employee?)employee.Clone();
+        return null;
+    }
+
+    private bool CheckId(int id, string message)
+    {
+        foreach (Employee employee in employees!)
+        {
+            if (employee.id == id)
+            {
+                Console.WriteLine($"Found Simular ID: {message}, cause {id} == {id}, Место занято {employee.name}");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void SetEmployee(int index, Employee employee)
+    {
+        if (CheckId(employee.id, $"Set {employee.name} Canceled")) return;
+        employees![index] = employee;
+    }
+
+    public void InsertEmplyee(int index, Employee employee)
+    {
+        if (CheckId(employee.id, $"Insert {employee.name} Canceled")) return;
+        employees!.Insert(index, employee);
+    }
+
+    public int GetAverageSalary()
+    {
+        int total = 0;
+        foreach (Employee employee in employees!) total += employee.salary;
+        return total / employees.Count();
+    }
+
+
+}
+
+class Employee : ICloneable
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public string? position { get; set; }
+    public string[]? skills { get; set; }
+    public int salary { get; set; }
+
+
+    public object Clone() => this.MemberwiseClone();
+
+}
+
+
+class Cinema
+{
+    public List<Film>? movies { get; set; }
+    public List<string>? genres { get; set; }
+    public Rating? ratings { get; set; }
+
+    public void AddMovie(Film movie) => movies?.Add(movie);
+    public void AddMovies(Film[] movies)
+    {
+        foreach (Film movie in movies) AddMovie(movie);
+    }
+
+    public void AddGenre(string genre) => genres?.Add(genre);
+
+}
+
+class Rating
+{
+    public int IMDb { get; set; }
+    public string? RottenTomatoes { get; set; }
+}
+
+
+class Director
+{
+    public string? name { get; set; }
+    public string? born { get; set; }
+}
+
+class Cast
+{
+    public string? name { get; set; }
+    public string? role { get; set; }
+}
+
+class Film
+{
+    public string? tittle { get; set; }
+    public int year { get; set; }
+    public Director? director { get; set; }
+    public Cast[]? cast { get; set; }
+}
+
+
+public class Program
+{
+    public static void PracticeC()
+    {
+        MyJSON<Cinema> CinemaJson = new("1.json");
+        Cinema cinema = CinemaJson.Read()!;
+
+        Director[] directors = [
+            new() { name="Jone Ban", born="1999-12-22" },
+            new() { name="Lilia Ann", born="1952-5-12" },
+            new() { name="VanGo Lan", born="1983-3-30" },
+            ];
+
+        Cast[] casts =
+        {
+            new() { name = "LoRence Mi", role = "Steve" },
+            new() { name = "Afgam Ni", role = "Freddy" },
+            new() { name = "Realf gotofon", role = "Ralf" }
+        };
+
+
+        Film[] films = [
+            new() { tittle = "Ralf", director = directors[0], cast = casts, year = 2017 },
+            new() { tittle = "Gigachad", director = directors[0], cast = casts, year = 1980 },
+            new() { tittle = "Meon", director = directors[0], cast = casts, year = 2001 },
+        ];
+
+        cinema.AddMovies(films);
+        CinemaJson.Write(cinema);
+
+        MyJSON<Company> RogsAndCamps = new("2.json");
+        Company company = RogsAndCamps.Read()!;
+
+        company.AddEmployee(new()
+        {
+            id = 100,
+            name = "Александр",
+            position = "Программист",
+            skills = ["Back-end", "Front-end", "Full-stack", "Soft-Skills"],
+            salary = 250_000
+        });
+
+        Employee employee = company.GetEmployee(1001)!;
+        employee.position = "Front-End";
+        //employee.id = 1002; // For error!
+        employee.id = 1004;
+        company.InsertEmplyee(2, employee);
+        //company.SetEmployee(0, employee); // or tak :)
+
+        RogsAndCamps.Write(company);
+    }
+
+
+    public static void Main()
+    {
+        PracticeC();
+    }
+
+}
+
+
